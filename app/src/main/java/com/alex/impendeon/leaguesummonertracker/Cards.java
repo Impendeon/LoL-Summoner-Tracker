a51570d66adc93688a624d3c2bbf6533c3127a1d
Alex Farra <farra.alexr@gmail.com>
2017-10-10 16:31:06 -0400

    Fifth Commit

---
+++ app/src/main/java/com/alex/impendeon/leaguesummonertracker/Cards.java
@@ -0,0 +1,19 @@
+package com.alex.impendeon.leaguesummonertracker;
+
+import android.widget.TextView;
+
+/**
+ * Created by alex on 10/10/17.
+ */
+
+public class Cards {
+    public TextView getTextView() {
+        return textView;
+    }
+
+    public void setTextView(TextView textView) {
+        this.textView = textView;
+    }
+
+    public TextView textView;
+}
